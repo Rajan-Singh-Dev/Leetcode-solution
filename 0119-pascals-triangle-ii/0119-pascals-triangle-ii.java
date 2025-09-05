a41dbdfbe1fdabf9861c3a763cd71cad27e9ad35
Rajan-Singh-Dev <abcrajan3@gmail.com>
2025-09-05 23:19:43 +0530

    Time: 1 ms (66.36%), Space: 41.1 MB (66.62%) - LeetHub

---
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        row.add(1);
+
+        for(int i=1; i<=rowIndex; i++){
+            //update from right to left to avoide overwriiten value we stille need
+            for(int j=row.size()-1; j>=1; j--){
+                row.set(j, row.get(j)+row.get(j-1));
+            }
+            row.add(1);
+        }
+
+        return row;
+        
+    }
+}
